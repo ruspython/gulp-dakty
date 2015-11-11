30ddeeb5d9167cf58f215128d6f79540a5a77a93
Vladislav <ruspython@gmail.com>
2015-11-11 19:44:24 +0300

    Prepend modules added

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var through = require('through2'),
     fs = require('fs'),
     path = require('path'),
     stream = require('stream'),
-    mkdirp = require('mkdirp')
+    mkdirp = require('mkdirp'),
+    buildify = require('buildify'),
+    concat = require('concat')
     ;
 
 const VAR_REGEX = /\/\*==([_\w]*).([_\w]*)==\*\//g;
@@ -61,14 +63,16 @@ function withReplacedVars(siteObject, body) {
     matchVar = match[match.length - 1]; // Variable of this key must be replaced
     while (match) {
         variable = siteObject[matchVar];
-        if (typeof variable !== 'object') {
-            if (typeof variable === 'string') {
-                variable = '"' + variable + '"'; // Surrounding with quote
-            } else if (typeof variable === 'undefined') {
-                variable = "null";
-            }
-            body = body.replace(match[0], "=" + variable);
-        } // Adding global variables
+        if (typeof variable === 'object') {
+            variable = JSON.stringify(variable);
+        }
+        else if (typeof variable === 'string') {
+            variable = '"' + variable + '"'; // Surrounding with quote
+        } else if (typeof variable === 'undefined') {
+            variable = "null";
+        }
+        body = body.replace(match[0], "=" + variable);
+        // Adding global variables
 
         match = VAR_REGEX.exec(body);
         matchVar = match ? match[match.length - 1] : '';
@@ -76,25 +80,42 @@ function withReplacedVars(siteObject, body) {
     return body;
 }
 
+function prependModules(filename, srcPath, siteObject) {
+    var modules = siteObject.modules,
+        moduleName,
+        modulePaths = []
+        ;
+
+    for (moduleName in modules) {
+        modulePaths.push(srcPath + modules[moduleName]);
+    }
+    modulePaths.push(filename);
+
+    concat(modulePaths, filename.replace('.tmp', ''), function () {});
+}
+
 module.exports = function (srcPath, destPath, daktConfig) {
-    var key
+    var siteName
         ;
 
     if (!(srcPath && destPath && daktConfig)) {
         throw new PluginError('gulp-dakty', 'Missing options for gulp-dakty');
     }
-    for (key in daktConfig) {
-        (function (key) {
-            mkDir(destPath + key)
+    //TODO: closure needs fix
+    for (siteName in daktConfig) {
+        (function (siteName) {
+            mkDir(destPath + siteName)
                 .then(function () {
                     return readSource(srcPath + 'daktyloskop.js')
                 })
                 .then(function (data) {
-                    data = withReplacedVars(daktConfig[key], data);
-                    return writeToDest(destPath + key + '/daktyloskop.js', data);
+                    data = withReplacedVars(daktConfig[siteName], data);
+                    return writeToDest(destPath + siteName + '/daktyloskop.tmp.js', data);
+                })
+                .then(function () {
+                    prependModules(destPath + siteName + '/daktyloskop.tmp.js', srcPath, daktConfig[siteName]);
                 })
             ;
-        }(key))
-
+        }(siteName))
     }
 };
